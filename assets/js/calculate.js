a03b803abba447ed8cb9dcbf2648c656cf603e70
James Pankhurst <jamespankhurst180@gmail.com>
2021-11-21 21:44:08 +0000

    First img upload

---
+++ assets/js/calculate.js
@@ -0,0 +1,31 @@
+/*==============================*/
+/*===== ORION DICE DECIDE ======*/
+/* www.jamespankhurst.co.uk/odd */
+/* jamespankhurst180@gmail.com  */
+/* github.com/jamespankhurst    */
+/* gitlab.com/jamespankhurst180 */
+/*==============================*/
+/*=================*/
+/*--- CALCULATE ---*/
+/*=================*/
+
+function calculate(dObj1, dObj2, dObj3, dObj4, dObj5, pid, gid, rd, tn) {
+
+
+	_handresult.gid = gid;
+	_handresult.pid = pid;
+	_handresult.rd = rd;
+	_handresult.pv = 0;
+	_handresult.hc = 'contact jamespankhurst180@gmail.com';
+	_handresult.tm = 'contact jamespankhurst180@gmail.com';
+	_handresult.d1 = 6;
+	_handresult.d2 = 6;
+	_handresult.d3 = 6;
+	_handresult.d4 = 6;
+	_handresult.d5 = 6;
+	
+	return (_handresult);
+
+}; 
+
+/*----- END CALCULATE -----*/
